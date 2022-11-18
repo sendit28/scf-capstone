6a4b0c7633c09950ce01c338d0112558b51e1b8c
+ <+>
2022-11-18 11:20:43 -0500

    Configured client-side routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,11 +12,15 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <h1>Page Count: {count}</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/testing" element={<h1>Test Route</h1>} />
+          <Route path="/" element={<h1>Page Count: {count}</h1>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
   
-
 export default App;
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'fallback/index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  # route to test your configuration
   get '/hello', to: 'application#hello_world'
+
+  get '*path',
+  to: 'fallback#index',
+  constraints: ->(req) { !req.xhr? && req.format.html? }
+
 end

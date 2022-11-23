34b64c1d7bdc050411304b1dfc6df118850a42aa
+ <+>
2022-11-23 12:13:33 -0500

    moves relation code from controller to models

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,5 @@
 class CategoriesController < ApplicationController
 
-  has_many :post_categories
-  has_many :posts, through: post_categories
+  
   
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,3 @@
 class UsersController < ApplicationController
-  has_secure :password
-
-  has_many :user_post_comments
-  has_many posts, through: :user_post_comments
-
-  validates :email, :username, presence: true
 
 end

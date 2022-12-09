0a1665604290101c473a3e0497515faf61484ef9
+ <+>
2022-12-08 23:43:52 -0500

    fixes syntax app cont

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ private
   end
 
   def render_unprocessable_entity_response(exception)
-    render json: { errors: exception.record.errors.full_messages }, status: :render_unprocessable_entity 
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity 
   end
 
 end

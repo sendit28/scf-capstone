310ccd804034b55856d7ad6eb8b9dad94ade455e
+ <+>
2022-12-09 17:16:42 -0500

    adds create crud

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,11 +9,11 @@ class PostsController < ApplicationController
   def create 
     # binding.pry
     post = Post.new(post_params)
-    post.id = @current_user.id
+    post.user_id = @current_user.id
     post.save
     render json: post, status: :created 
   end
-
+  # binding.pry
   def destroy
     post = Post.find_by(id: params[:id])
     post.destroy

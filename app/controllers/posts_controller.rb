2b05aa268e589353d679148306b9a03d4febb1bc
+ <+>
2022-12-08 23:50:50 -0500

    creates post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,30 @@
 class PostsController < ApplicationController
 
+  skip_before_action :authorize, only: :index
+
+  def index 
+    render json: Post.all 
+  end
+
+  def create 
+    # binding.pry
+    post = Post.new(post_params)
+    post.id = @current_user.id
+    post.save
+    render json: post, status: :created 
+  end
+
+  def destroy
+    post = Post.find_by(id: params[:id])
+    post.destroy
+    # head :no_content
+    render json: post, status: :ok
+  end
+
+  private
+
+  def post_params
+    params.permit(:title, :date, :description)
+  end
+
 end

f03d3d0bfe0fa7a28ca731e6b701c7a779f6aa6c
+ <+>
2022-12-08 23:48:19 -0500

    creates app

---
+++ client/src/components/App.js
@@ -0,0 +1,85 @@
+// import './App.css';
+import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import SignUpForm from "./SignUpForm";
+import LoginForm from "./LoginForm";
+import { useNavigate } from "react-router-dom";
+import PostCard from "./PostCard";
+import PostForm from "./PostForm";
+
+// import Login from '../pages/Login';
+
+function App() {
+  const [user, setUser] = useState(null);
+  const [posts, setPosts] = useState([]);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) =>{
+          setUser(user)
+          // (...user)(data)
+        });
+      }
+    });
+  }, []);
+
+  // useEffect(() => {
+  //   fetch("/posts")
+  //    .then((r) =>r.json())
+  //    .then((data) => setPosts(data));
+  // }, []);
+
+  // if (!user) return <Login onLogin={setUser} />
+
+  
+  const handleLogoutClick = () => {
+    fetch("/logout", {
+      method: "DELETE",
+    }).then((r) => {
+      if (r.ok) {
+        setUser(null);
+        navigate("/");
+      }
+    });
+  };
+
+  function handleDeletePost(deletedPost) {
+    const stateCopy = JSON.parse(JSON.stringify(posts))
+    const updatedPosts = stateCopy.filter((post) => post.id !== deletedPost.id);
+    setPosts(updatedPosts);
+  }
+
+  return (
+    <div className="App">
+      <button onClick={handleLogoutClick}>Logout</button>
+      <Routes>
+        <Route path="/signup" element={<SignUpForm setUser={setUser} />} />
+        {/* <Route path="/login" element={<LoginForm setUser={setUser} />} /> */}
+
+        {/* <Route path="/testing" element={<h1>Test Route</h1>} /> */}
+        <Route
+          path="/"
+          element={<LoginForm setUser={setUser} setPosts={setPosts} />}
+        />
+        <Route
+          path="/posts"
+          element={
+            <PostCard posts={posts} handleDeletePost={handleDeletePost} />
+          }
+        />
+        <Route
+          path="/posts/new"
+          element={
+            <PostForm setPosts={setPosts} user={user} />
+          }
+        />
+        
+      </Routes>
+    </div>
+  );
+}
+
+export default App;

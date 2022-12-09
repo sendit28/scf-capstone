7cb5b89d2ebc4e3053281cf3e0d69245b85d8963
+ <+>
2022-12-08 23:50:01 -0500

    creates postform

---
+++ client/src/components/PostForm.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+
+function PostForm({ setPosts, user }) {
+  const navigate = useNavigate()
+  const initialState = {
+    title: "",
+    date: "",
+    description: "",
+  }
+
+  const [formData, setFormData] = useState(initialState)
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    // const stateCopy = JSON.parse(JSON.stringify(posts))
+    const formDataCopy = {...formData, user_id: user.id}
+    // const formDataCopy = JSON.parse(JSON.stringify({ ...formData, user_id: user.id}))
+
+    fetch("/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(formDataCopy)
+    })
+      .then(r=> r.json())
+      .then(data => setPosts((posts) => [...posts, data]))
+      navigate("/posts")
+      setFormData(initialState)
+  }
+  
+  
+  return (
+    <div className="post-form">
+      <h2> Give me your Show and Tell </h2>
+      <form onSubmit={handleSubmit}>
+        <label> Title </label>
+        <input name="title" type="text" onChange={handleChange} value={formData.title}/>
+        <label> Date </label>
+        <input name="date" type="text" onChange={handleChange} value={formData.date}/>
+        <label> Post </label>
+        <input name="description" type="text" onChange={handleChange} value={formData.description}/>
+        <input type="submit"></input>
+      </form>
+    </div>
+    )
+}
+
+export default PostForm

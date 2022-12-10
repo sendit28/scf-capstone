d6dcf7b844527d2fdfa9ea313893f29414a050f6
+ <+>
2022-12-10 11:18:28 -0500

    adjusts post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   skip_before_action :authorize, only: :index
 
   def index 
-    render json: Post.all 
+    render json: Post.all, status: :ok
   end
 
   def create 
@@ -13,6 +13,18 @@ class PostsController < ApplicationController
     post.save
     render json: post, status: :created 
   end
+
+  def show
+    post = Post.find_by(id: params[:id])
+    render json: post, status: :ok
+  end
+
+  def update
+    post = Post.find_by(id: params[:id])
+    post.update(post_params)
+    render json: post, status: :accepted
+  end
+
   # binding.pry
   def destroy
     post = Post.find_by(id: params[:id])

afa5c4a954713f0a6aff2091d3c1d1efd3e509ee
+ <+>
2022-12-14 14:30:39 -0500

    add navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import { Link } from "react-router-dom"
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function NavBar({handleLogoutClick}) {
 
   return (
-    <div className="nav-bar">
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Show and Tell</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link link to="/posts">Posts</Nav.Link>
+            <Nav.Link href="/posts/new">Publish</Nav.Link>
+            <Nav.Link href="/logout">Logout</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default NavBar
+
+{/* <div className="nav-bar">
       <h1> Show and Tell </h1>
       <div>
         <Link to="/posts"> Posts </Link>
@@ -15,8 +38,4 @@ function NavBar({handleLogoutClick}) {
       <div>
         <Link to="/" onClick={handleLogoutClick}> Logout </Link>
       </div>
-    </div>
-  )
-}
-
-export default NavBar
+    </div> */}

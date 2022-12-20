857fc2fbf816db3c8b00507c77aae95ddb32bdc6
+ <+>
2022-12-19 22:54:34 -0500

    adds image input

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import {
   MDBBtn,
   MDBContainer,
@@ -7,54 +7,54 @@ import {
   MDBCol,
   MDBIcon,
   MDBInput,
-  MDBTextArea
+  MDBCardImage,
+  MDBTextArea,
 } from "mdb-react-ui-kit";
 
-
 function PostForm({ setPosts, user, posts }) {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const initialState = {
     title: "",
     date: "",
+    img_url: "",
     description: "",
-  }
+  };
 
-  const [formData, setFormData] = useState(initialState)
+  const [formData, setFormData] = useState(initialState);
 
   const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value
-    })
-  }
+      [e.target.name]: e.target.value,
+    });
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    const formDataCopy = {...formData, user_id: user.id}
-    
+    e.preventDefault();
+    const formDataCopy = { ...formData, user_id: user.id };
+
     fetch("/posts", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(formDataCopy)
+      body: JSON.stringify(formDataCopy),
     })
-      .then(r=> r.json())
-      .then(data => {
-        setPosts((posts) => [...posts, data])
-      })
-      navigate("/posts")
-      setFormData(initialState)
-  }
-  
-  
+      .then((r) => r.json())
+      .then((data) => {
+        setPosts((posts) => [...posts, data]);
+      });
+    navigate("/posts");
+    setFormData(initialState);
+  };
+
   return (
     <MDBContainer fluid>
       <MDBRow>
         <MDBCol sm="6">
           <div className="d-flex flex-row ps-5 pt-5">
             {/* <MDBIcon fas icon="crow fa-3x me-3" style={{ color: '#709085' }}/> */}
-            <span className="h1 fw-bold mb-0">Share your Show and Tell</span>
+            <span className="h1 fw-bold mb-0">Begin your Show and Tell</span>
           </div>
           <form onSubmit={handleSubmit}>
             <div className="d-flex flex-column justify-content-center h-custom-2 w-75 pt-4">
@@ -62,7 +62,7 @@ function PostForm({ setPosts, user, posts }) {
                 className="fw-normal mb-3 ps-5 pb-3"
                 style={{ letterSpacing: "1px" }}
               >
-                Create
+                What would you like to share?
               </h3>
 
               <MDBInput
@@ -75,7 +75,7 @@ function PostForm({ setPosts, user, posts }) {
                 onChange={handleChange}
                 value={formData.title}
               />
-              <MDBInput
+              {/* <MDBInput
                 wrapperClass="mb-4 mx-5 w-100"
                 label="Date"
                 id="formControlLg"
@@ -84,8 +84,19 @@ function PostForm({ setPosts, user, posts }) {
                 size="lg"
                 onChange={handleChange}
                 value={formData.date}
+              /> */}
+              <MDBInput
+                wrapperClass="mb-4 mx-5 w-100"
+                label="Image"
+                id="formControlLg"
+                name="img_url"
+                type="text"
+                size="lg"
+                onChange={handleChange}
+                value={formData.img_url}
               />
-              <MDBTextArea rows={6}
+              <MDBTextArea
+                rows={6}
                 wrapperClass="mb-4 mx-5 w-100"
                 label="Post"
                 id="formControlLg"
@@ -96,13 +107,20 @@ function PostForm({ setPosts, user, posts }) {
                 value={formData.description}
               />
 
-              <MDBBtn className="mb-4 px-5 mx-5 w-100" color="info" size="md" type="submit"> Publish
+              <MDBBtn
+                className="mb-4 px-5 mx-5 w-100"
+                color="info"
+                size="md"
+                type="submit"
+              >
+                {" "}
+                Publish
               </MDBBtn>
               {/* {errors.map((err) => <div key={err} style={{color: "red"}}>{err}</div>)} */}
               {/* <p className="ms-5">
                 Don't have an account?{" "}
                 <Link to="/signup">Signup Here</Link> */}
-                {/* <a href="#!" class="link-info">
+              {/* <a href="#!" class="link-info">
                   Register here
                 </a> */}
               {/* </p> */}
@@ -120,12 +138,13 @@ function PostForm({ setPosts, user, posts }) {
         </MDBCol>
       </MDBRow>
     </MDBContainer>
-    )
+  );
 }
 
-export default PostForm
+export default PostForm;
 
-{/* <div className="post-form">
+{
+  /* <div className="post-form">
       <h2> Share your Show and Tell </h2>
       <form onSubmit={handleSubmit}>
         <label> Title </label>
@@ -136,4 +155,5 @@ export default PostForm
         <input name="description" type="text" onChange={handleChange} value={formData.description}/>
         <input type="submit"></input>
       </form>
-    </div> */}
+    </div> */
+}
